3031f5ad7a2115d82fb54cbc70b32fe4cc8d7b77
amirborzouie <amirborzouie@yahoo.com>
2021-06-26 23:55:26 +0430

    a simple script is made

--- 2/roles/myproject/files/schedule.sh
+++ 2/roles/myproject/files/schedule.sh
@@ -10,13 +10,13 @@ echo `date` >> /home/schedule.txt
 echo "Current Date is: $Day-$Month-$Year" >> /home/schedule.txt
 echo "Current Time is: $Hour:$Minute:$Second" >> /home/schedule.txt
 
-echo -n "Enter your schedule:"
-read schedule
+#echo -n "Enter your schedule:"
+#read schedule
 
-echo "You Entered: $schedule"
-echo """RECORDED TASK IS:
-          $schedule """ >> /home/schedule.txt
-echo """_____________________________________________________________________________
-"""  >> /home/schedule.txt
+#echo "You Entered: $schedule"
+#echo """RECORDED TASK IS:
+#          $schedule """ >> /home/schedule.txt
+#echo """_____________________________________________________________________________
+#"""  >> /home/schedule.txt
 
-cat /home/schedule.txt
+#cat /home/schedule.txt
